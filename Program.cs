086c859902c594eadbc31620b5be20183561cfac
329 <329@329-11>
2022-03-01 11:33:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car n = new Car(10, 65, "B");
+            n.MovingCar(400);
+            n.Show();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Transport.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Car
+    {
+        double fuelconsumption;
+        double tanksize;
+        string category;
+        public Car(double consumption, double tanksize, string category)
+        {
+            fuelconsumption = consumption / 100;
+            this.tanksize = tanksize;
+            this.category = category;
+        }
+        public void MovingCar(int move)
+        {
+            double remainingfuel = tanksize;
+            remainingfuel -= fuelconsumption * move;
+            Console.Write("Остаток бензина: " + remainingfuel);
+            double powerreserve = (remainingfuel / (fuelconsumption));
+            Console.WriteLine($"    Запас хода: {powerreserve:0.00} км");
+        }
+        public void Show() => Console.WriteLine($"Категория прав: {category}");
+    }
+    class Plane
+    {
+        double fuelconsumption;
+        double tanksize;
+        string category = "Лицензия на управление самолетом";
+        public Plane(double consumption, double tanksize, string category)
+        {
+            fuelconsumption = consumption / 100;
+            this.tanksize = tanksize;
+        }
+        public void MovingPlane(int move)
+        {
+            double remainingfuel = tanksize;
+            remainingfuel -= fuelconsumption * move;
+            Console.Write("Остаток бензина: " + remainingfuel);
+            double powerreserve = (remainingfuel / (fuelconsumption));
+            Console.WriteLine($"    Запас хода: {powerreserve:0.00} км");
+        }
+        public void Show() => Console.WriteLine($"Категория прав: {category}");
+    }
+    class Boat
+    {
+        double fuelconsumption;
+        double tanksize;
+        string category = "Моторное судно";
+        public Boat(double consumption, double tanksize)
+        {
+            fuelconsumption = consumption / 100;
+            this.tanksize = tanksize;
+        }
+        public void MovingBoat(int move)
+        {
+            double remainingfuel = tanksize;
+            remainingfuel -= fuelconsumption * move;
+            Console.Write("Остаток бензина: " + remainingfuel);
+            double powerreserve = (remainingfuel / (fuelconsumption));
+            Console.WriteLine($"    Запас хода: {powerreserve:0.00} км");
+        }
+        public void Show() => Console.WriteLine($"Категория прав: {category}");
+    }
+}
+
